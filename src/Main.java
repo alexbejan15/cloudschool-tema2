11122332aef3529bf90e025f95f807fe968800ec
Alexandru Bejan <alex.bejan11@gmail.com>
2022-07-22 23:25:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        TrafficLight trafficLight = new TrafficLight();
+
+        Runnable carNorthSouth = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Car passed on the North-South road.");
+            }
+        };
+
+        Runnable carEastWest = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Car passed on the East-West road.");
+            }
+        };
+        
+        trafficLight.carArrived( 1, carNorthSouth, carEastWest);
+        trafficLight.carArrived( 1, carNorthSouth, carEastWest);
+        trafficLight.carArrived( 2, carNorthSouth, carEastWest);
+        trafficLight.carArrived( 2, carNorthSouth, carEastWest);
+        trafficLight.carArrived( 2, carNorthSouth, carEastWest);
+        trafficLight.carArrived( 2, carNorthSouth, carEastWest);
+        trafficLight.carArrived( 1, carNorthSouth, carEastWest);
+        trafficLight.carArrived( 1, carNorthSouth, carEastWest);
+        trafficLight.carArrived( 1, carNorthSouth, carEastWest);
+    }
+}
---
+++ src/TrafficLight.java
@@ -0,0 +1,46 @@
+import java.util.concurrent.Semaphore;
+
+class TrafficLight {
+
+    private Semaphore greenLight;
+    private boolean roadEastWest;
+    private boolean roadNorthSouth;
+
+    public TrafficLight() {
+        this.greenLight = new Semaphore(1, true);
+        this.roadEastWest = true;
+        this.roadNorthSouth = false;
+    }
+
+    public void carArrived(int roadId, Runnable carNorthSouth, Runnable carEastWest) {
+        try {
+            greenLight.acquire();
+           
+            if ((roadId == 1 && roadEastWest)) {
+                carEastWest.run();
+            }
+            
+            else if (roadId == 2 && roadNorthSouth) {
+                carNorthSouth.run();
+            }
+            
+            else if (roadId == 1 && !roadEastWest) {
+                System.out.println("Red light on North-South --> Green light on East-West");
+                roadEastWest = true;
+                roadNorthSouth = false;
+                carEastWest.run();
+            } 
+            
+            else if (roadId == 2 && !roadNorthSouth) {
+                System.out.println("Red light on East-West --> Green light on North-South");
+                roadNorthSouth = true;
+                roadEastWest = false;
+                carNorthSouth.run();
+            }
+            
+            greenLight.release();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
